fdd92384fcbeaa3d1830da5dc0e3ddc5e741e9af
Ryan Pangrle <rpangrle@microsoft.com>
2017-07-11 14:21:54 -0700

    Initial project file

---
+++ RKPWebAppKit/RKPWebAppKit.h
@@ -0,0 +1,19 @@
+//
+//  RKPWebAppKit.h
+//  RKPWebAppKit
+//
+//  Created by Ryan Pangrle on 7/11/17.
+//  Copyright © 2017 RKP. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for RKPWebAppKit.
+FOUNDATION_EXPORT double RKPWebAppKitVersionNumber;
+
+//! Project version string for RKPWebAppKit.
+FOUNDATION_EXPORT const unsigned char RKPWebAppKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RKPWebAppKit/PublicHeader.h>
+
+
